861eda90b36371934d234761c321a8469d5766e8
Berkus Decker <berkus@atta-metta.net>
2015-07-17 10:01:29 +0300

    Add redis-postgres tool prototype.

---
+++ orm.js
@@ -0,0 +1,117 @@
+"use strict";
+import _ from 'lodash'
+
+// @todo Move this to DbConfig
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host     : '127.0.0.1',
+    user     : 'pepyatka',
+    password : 'pepyatka',
+    database : 'pepyatka',
+    charset  : 'utf8'
+  }
+})
+
+var bookshelf = require('bookshelf')(knex)
+exports.Bookshelf = bookshelf
+
+bookshelf.Model.prototype.parse = function(attrs) {
+  return _.reduce(attrs, function(memo, val, key) {
+    memo[_.camelCase(key)] = val;
+    return memo;
+  }, {});
+}
+
+bookshelf.Model.prototype.format = function(attrs) {
+  return _.reduce(attrs, function(memo, val, key) {
+    memo[_.snakeCase(key)] = val;
+    return memo;
+  }, {});
+}
+
+var User = bookshelf.Model.extend({
+  tableName: 'users',
+  idAttribute: 'uuid',
+  hasTimestamps: ['createdAt', 'updatedAt']
+  // roles: function() {
+  //   return this.hasMany(Role);
+  // }
+})
+exports.User = User
+
+var Timeline = bookshelf.Model.extend({
+  tableName: 'timelines',
+  idAttribute: 'uuid',
+
+//   posts: function() {
+//     return this.belongsToMany(Post, 'timelines_posts')
+//   }
+})
+exports.Timeline = Timeline
+
+var Role = bookshelf.Model.extend({
+  tableName: 'roles',
+//   users: function() {
+//     return this.belongsToMany(User)
+//   }
+})
+exports.Role = Role
+
+var Post = bookshelf.Model.extend({
+  tableName: 'posts',
+  idAttribute: 'uuid',
+  hasTimestamps: ['createdAt', 'updatedAt']
+
+  timelines: function() {
+    return this.belongsToMany(Timeline, 'timelines_posts')
+  },
+  user: function() {
+    return this.belongsToOne(User)
+  }
+})
+exports.Post = Post
+
+var Like = bookshelf.Model.extend({
+  tableName: 'likes',
+  idAttribute: 'uuid',
+  hasTimestamps: ['createdAt', 'updatedAt']
+//   user: function() {
+//     return this.belongsTo(User)
+//   },
+//   post: function() {
+//     return this.belongsTo(Post, 'emotions').withPivot(['meta'])
+//   }
+})
+exports.Like = Like
+
+var Comment = bookshelf.Model.extend({
+  tableName: 'comments',
+  idAttribute: 'uuid',
+  hasTimestamps: ['createdAt', 'updatedAt']
+//   post: function() {
+//     return this.belongsTo(Post, 'posts_comments').withPivot(['meta'])
+//   }
+})
+exports.Comment = Comment
+
+var Attachment = bookshelf.Model.extend({
+  tableName: 'attachments',
+  idAttribute: 'uuid',
+  hasTimestamps: ['createdAt', 'updatedAt']
+//   post: function() {
+//     return this.belongsTo(Post, 'posts_comments').withPivot(['meta'])
+//   }
+})
+exports.Attachment = Attachment
+
+// var ChangesHistoryItem = bookshelf.Model.extend({
+//   tableName: 'changes_history',
+//   idAttribute: 'uuid'
+// })
+// exports.ChangesHistoryItem = ChangesHistoryItem
+
+// var ChangesHistory = bookshelf.Collection.extend({
+//   model: ChangesHistoryItem
+// })
+// exports.ChangesHistory = ChangesHistory
---
+++ redis2pq.js
@@ -0,0 +1,98 @@
+//
+// Grab data from redis and populate it to PostgreSQL
+//
+"use strict";
+import express from 'express'
+import environment from './config/environment'
+import _ from 'lodash'
+import redis from './config/database'
+import models from './app/models'
+import orm from './orm'
+
+process.env.NODE_ENV = "development"
+
+var run = async function() {
+  console.log(`Start`)
+  var app = express()
+  console.log(`Start2`)
+  var app = await environment.init(app)
+  console.log(`Start3`)
+  var database = redis.connect()
+  console.log(`Start4`)
+
+  var usernames = await database.keysAsync('username:*')
+  console.log(`Start5`)
+  var userIds = await* usernames.map((username) => database.getAsync(username))
+  console.log(`Start6`)
+  var users = await* userIds.map((userId) => models.FeedFactory.findById(userId))
+  console.log(`Start7`)
+  var timelines = await* users.map((user) => user.getPostsTimeline())
+  console.log(`Start8`)
+
+  await* users.map(async function(user) {
+    console.log(`Start9`)
+    var u = orm.User.forge({
+      username: user.username,
+      screenName: user.screenName,
+      hashedPassword: user.hashedPassword,
+      email: user.email,
+      isPrivate: user.isPrivate,
+      type: user.type,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      deletedAt: null,
+      tokens: {},
+      bans: []
+    })
+    console.log(`Start10`)
+    u.save({id: user.id})
+    console.log(`User ${u.id} saved`)
+  })
+  console.log(`Start11`)
+
+  await* timelines.map(async function(timeline) {
+    console.log(`Start12`)
+    var t = orm.Timeline.forge({
+      type: 'Posts'
+    })
+    console.log(`Start13`)
+    await t.save({id: timeline.id})
+    console.log(`Timeline ${t.id} saved`)
+
+    let posts = await timeline.getPosts(0, 10)
+    console.log(`Start14`)
+    await* posts.map(async function(post) {
+      console.log(JSON.stringify(post))
+      var p = orm.Post.forge({
+        body: post.body,
+        user: post.userId
+      })
+      console.log(`Start15`)
+      await p.save({id: post.id})
+      console.log(`Post ${p.id} saved`)
+  //     let postedTo = await post.getPostedToIds()
+  //     console.log('Checking post ' + post.id + '.')
+  //     if (postedTo.length === 0) {
+  //       let user = await models.User.findById(post.userId)
+  //       let key = await user.getPostsTimelineId()
+  //       let to = mkKey(["post", post.id, "to"])
+  //       console.log('Need to set ' + to + ' to ' + key + '.')
+  //       await database.sadd(to, key)
+  //       console.log('Fixed.')
+  //     } else {
+  //       console.log('OK.')
+  //     }
+    })
+  })
+
+  console.log('Done.')
+  process.exit()
+}
+
+console.log(`Start16`)
+try {
+  run()
+} catch(e) {
+  console.log(`Error: ${e}`)
+}
+console.log(`Start17`)
